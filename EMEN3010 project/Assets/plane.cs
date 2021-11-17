af96717ab08da58d73f063389874870e469a5e16
Hyh2001 <55273583+Hyh2001@users.noreply.github.com>
2021-11-17 19:50:08 +0800

    code

--- EMEN3010 project/Assets/plane.cs
+++ EMEN3010 project/Assets/plane.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class plane : MonoBehaviour
 {
+    Rigidbody2D plane;
+    float force = 200.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        plane = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            plane.AddForce
+        }
     }
 }
