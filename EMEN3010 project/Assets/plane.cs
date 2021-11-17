33d2dc4e9f01eeda9a2fec454fab852460457c36
Hyh2001 <55273583+Hyh2001@users.noreply.github.com>
2021-11-17 20:40:29 +0800

    move plane

--- EMEN3010 project/Assets/plane.cs
+++ EMEN3010 project/Assets/plane.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class plane : MonoBehaviour
 {
-    Rigidbody2D plane;
-    float force = 200.0f;
-
+    Rigidbody2D plane1;
+    float force = 50.0f;
+    float speed = 50.0f;
     // Start is called before the first frame update
     void Start()
     {
-        plane = GetComponent<Rigidbody2D>();
+        plane1 = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        int direction = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            plane.AddForce
+            direction = 1;
+            plane1.AddForce(transform.right * direction * speed);
+            //      plane1.velocity.x = direction * speed;
+
         }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+            plane1.AddForce(transform.right * direction * speed);
+            //          plane1.velocity.x = direction * speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = 1;
+            plane1.AddForce(transform.up * direction * speed);
+            //      plane1.velocity.x = direction * speed;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = -1;
+            plane1.AddForce(transform.up * direction * speed);
+            //      plane1.velocity.x = direction * speed;
+
+        }
+        plane1.velocity = Vector2.zero;
+        /*        if (Input.GetKeyUp(KeyCode.RightArrow || KeyCode.LeftArrow || KeyCode.UpArrow ||  KeyCode.DownArrow))
+                {
+
+                    plane1.velocity = Vector2.zero;
+                }*/
     }
 }
