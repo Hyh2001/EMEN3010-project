c8b19420598d552946a0b70d8ae066443b533bc3
JasonMok <1580957383@qq.com>
2021-11-22 21:48:08 +0800

    1

--- EMEN3010 project/Assets/ClearDirector.cs
+++ EMEN3010 project/Assets/ClearDirector.cs
@@ -7,7 +7,7 @@ public class ClearDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.GetKey("return"))
         {
             SceneManager.LoadScene("scene1");
         }
